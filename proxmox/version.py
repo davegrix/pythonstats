3df28435ff706fc48cdf4f56d26825e01811b820
Dave <dave@dgrix.com>
2017-06-25 20:32:49 +0100

    Move proxmox into new folder

--- proxmox/version.py
+++
@@ -1,9 +0,0 @@
-from proxmox import connect
-
-
-class Version:
-    def __init__(self, auth):
-        self.conn = connect.Connect(auth)
-
-    def get_version(self):
-        return self.conn.connect('version')
