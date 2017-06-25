d8073b0231772abd1c31b0b179587602a8739bcf
Dave <dave@dgrix.com>
2017-06-25 20:26:25 +0100

    Access added, proxmox complete

---
+++ proxmox/access.py
@@ -0,0 +1,44 @@
+from proxmox import connect
+
+
+class Access:
+    def __init__(self, auth):
+        self.conn = connect.Connect(auth)
+
+    def get_access(self):
+        return self.conn.connect('access')
+
+    # domains
+    def get_access_domains(self):
+        return self.conn.connect('access/domains')
+
+    def get_access_domains_by_realm(self, realm):
+        return self.conn.connect('access/domains/' + realm)
+
+    # groups
+    def get_access_groups(self):
+        return self.conn.connect('access/groups')
+
+    def get_access_groups_by_id(self, id):
+        return self.conn.connect('access/groups/' + id)
+
+    # roles
+    def get_access_roles(self):
+        return self.conn.connect('access/roles')
+
+    def get_access_roles_by_id(self,id):
+        return self.conn.connect('access/roles/' + id)
+
+    # users
+    def get_access_users(self):
+        return self.conn.connect('access/users')
+
+    def get_access_users_by_id(self, userid):
+        return self.conn.connect('access/users/' + userid)
+
+    def get_access_acl(self):
+        return self.conn.connect('acl')
+
+    def get_access_ticket(self):
+        return self.conn.connect('ticket')
+
